fbbbf054120f0fd7b1b530cfdea59e8091b0bf57
Cem Daloglu <chemdaloglu@gmail.com>
2022-08-28 00:13:02 +0200

    update

--- Software_Practical.py
+++ Software_Practical.py
@@ -90,12 +90,13 @@ def vor_plotting(coords: list, rand_point_no: int, region_polys: dict, boundary_
     plt.close()
 
 
-def vor_plotting_rgb(filename: str, rand_point_no: int, region_polys: dict, img_width: int,
-                     img_height: int, line_width: int, line_color: str, img_no: int) -> None:
+def vor_plotting_rgb(dataset_path: str, filename: str, rand_point_no: int, region_polys: dict, img_width: int,
+                     img_height: int, line_width: int, line_color: str, img_no: int, color_list: list) -> None:
     '''
     This function saves a colored .png image with the desired line width, color, and name.
 
-    :param filename: Name of the file. Filename will be Image_rgb"filename" img_no.png
+    :param dataset_path: Path of the dataset folder
+    :param filename: Name of the file. Filename will be "Image_rgb"
     :param rand_point_no: Number of random points.
     :param region_polys: Dictionary of polygon objects size of the number of random points. Each element of the
       dictionary contains the coordinates of the corner points.
@@ -104,7 +105,32 @@ def vor_plotting_rgb(filename: str, rand_point_no: int, region_polys: dict, img_
     :param line_width: Width of the line, 0 means no line.
     :param line_color: Color of the line.
     :param img_no: The number that the current image will be saved with a name with that number.
+    :param color_list: list of colors that the constructed image's Voronoi cells' will use
     '''
+
+    fig, ax = subplot_for_map(figsize=(img_width / 120, img_height / 120))
+    rand_color = []
+    for point_no in range(rand_point_no):
+        points = []
+        for p_idx in range(len(region_polys[point_no].exterior.coords.xy[0])):
+            points.append([region_polys[point_no].exterior.coords.xy[0][p_idx],
+                           region_polys[point_no].exterior.coords.xy[1][p_idx]])
+        points = np.array(tuple(points))
+        plot_line(ax, points, linewidth=line_width, color=line_color)
+        if filename == '_mock':
+            rand_color.append(random.sample(list(matplotlib.colors.CSS4_COLORS), k=1))
+        else:
+            rand_color.append(random.sample(color_list, k=1))
+    for point_no in range(rand_point_no):
+        plot_polygon_collection_with_color(ax, [region_polys[point_no]], color=rand_color[point_no])
+    plt.gca().set_axis_off()
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    plt.margins(0, 0)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.savefig(dataset_path + '/Image_rgb' + filename + ' {}'.format(img_no + 1) + '.png', dpi=120, format='png', bbox_inches='tight',
+                pad_inches=0)
+    plt.close()
 
     fig, ax = subplot_for_map(figsize=(img_width / 120, img_height / 120))
     for point_no in range(rand_point_no):
@@ -113,17 +139,13 @@ def vor_plotting_rgb(filename: str, rand_point_no: int, region_polys: dict, img_
             points.append([region_polys[point_no].exterior.coords.xy[0][p_idx],
                            region_polys[point_no].exterior.coords.xy[1][p_idx]])
         points = np.array(tuple(points))
-        plot_line(ax, points, linewidth=line_width, color=line_color)
-    rand_color = random.sample(list(matplotlib.colors.CSS4_COLORS), k=rand_point_no)
-    for point_no in range(rand_point_no):
-        plot_polygon_collection_with_color(ax, [region_polys[point_no]], color=rand_color[point_no])
-    plt.gca().set_axis_off()
+        plot_line(ax, points, linewidth=line_width, color='black')
+        plt.gca().set_axis_off()
     plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
     plt.margins(0, 0)
     plt.gca().xaxis.set_major_locator(plt.NullLocator())
     plt.gca().yaxis.set_major_locator(plt.NullLocator())
-    plt.savefig('Image_rgb' + filename + ' {}'.format(img_no + 1) + '.png', dpi=120, format='png', bbox_inches='tight',
-                pad_inches=0)
+    plt.savefig(dataset_path + '/line {}'.format(img_no + 1) + '.png', dpi=120, format='png', bbox_inches='tight', pad_inches=0)
     plt.close()
 
 
@@ -211,46 +233,49 @@ def img_to_tensor_colored(img: str, img_height: int, img_width: int) -> torch.Te
     return tensor
 
 
-def tensor_out_bw(total_img: int, img_height: int, img_width: int) -> torch.Tensor:
+def tensor_out_bw(file_name: str, total_img: int, img_height: int, img_width: int) -> torch.Tensor:
     '''
     Takes the total number of bw images and returns the tensor of each image.
 
+    :param file_name: Name of the file. Filename will be "Image_rgb" or "line"
     :param total_img: Filename of the image whose tensors will be returned.
     :param img_height: Height of the image.
     :param img_width: Width of the image.
     :return tensor: Tensor of the all images, return shape is (total_img, 1, img_width, img_height)
     '''
 
-    output_bw = img_to_tensor_bw('Image {}'.format(1) + '.png', img_height, img_width)
+    output_bw = img_to_tensor_bw(file_name + ' {}'.format(1) + '.png', img_height, img_width)
     for idx in range(total_img - 1):
-        output_bw = torch.cat((output_bw, img_to_tensor_bw('Image {}'.format(idx + 2) + '.png', img_height, img_width)))
+        output_bw = torch.cat((output_bw, img_to_tensor_bw(file_name + ' {}'.format(idx + 2) + '.png', img_height, img_width)))
     output_bw = output_bw.reshape(total_img, 1, img_width, img_height)
     return output_bw
 
 
-def tensor_out_colored(total_img: int, img_height: int, img_width: int) -> torch.Tensor:
+def tensor_out_colored(file_name: str, total_img: int, img_height: int, img_width: int) -> torch.Tensor:
     '''
     Takes the total number of colored images and returns the tensor of each image.
 
+    :param file_name: Name of the file. Filename will be "Image_rgb"
     :param total_img: Filename of the image whose tensors will be returned.
     :param img_height: Height of the image.
     :param img_width: Width of the image.
     :return tensor: Tensor of the all images, return shape is (total_img, 3, img_width, img_height)
     '''
 
-    output_rgb = img_to_tensor_colored('Image_rgb {}'.format(1) + '.png', img_height, img_width)
+    output_rgb = img_to_tensor_colored(file_name + ' {}'.format(1) + '.png', img_height, img_width)
     for idx in range(total_img - 1):
         output_rgb = torch.cat(
-            (output_rgb, img_to_tensor_colored('Image_rgb {}'.format(idx + 2) + '.png', img_height, img_width)))
+            (output_rgb, img_to_tensor_colored(file_name + ' {}'.format(idx + 2) + '.png', img_height, img_width)))
     output_rgb = output_rgb.reshape(total_img, 3, img_width, img_height)
     return output_rgb
 
 
-def tensor_out_2(file_name: str, total_img: int, img_height: int, img_width: int, line_width: int) -> torch.Tensor:
+def tensor_out_2(dataset_path: str, file_name: str, total_img: int, img_height: int, img_width: int, line_width: int) -> torch.Tensor:
     '''
     Takes the total number of colored images and returns a tensor which contains unique values for each Voronoi
     pixel, and 0 for the border pixels.
 
+    :param dataset_path: Path of the dataset folder
     :param file_name: Name of the file. Filename will be Image_rgb"filename" img_no.png
     :param total_img: Filename of the image whose tensors will be returned.
     :param img_height: Height of the image.
@@ -258,7 +283,12 @@ def tensor_out_2(file_name: str, total_img: int, img_height: int, img_width: int
     :param line_width: Width of the line.
     :return tensor (): Tensor of the all images, return shape is (total_img, img_width, img_height)
     '''
-    img_gray_mode = cv2.imread('Image_rgb' + file_name + ' {}'.format(1) + '.png', 0)
+    line_tensor = tensor_out_bw(dataset_path + '/line', total_img, img_height, img_width)
+    indices = line_tensor > 0
+    line_tensor[indices] = 1
+    line_tensor = line_tensor.reshape(total_img, img_width, img_height)
+
+    img_gray_mode = cv2.imread(dataset_path + '/Image_rgb' + file_name + ' {}'.format(1) + '.png', 0)
     img_gray_mode[:line_width, :] = 0
     img_gray_mode[:, :line_width] = 0
     img_gray_mode[img_gray_mode.shape[0] - line_width:, :] = 0
@@ -267,7 +297,7 @@ def tensor_out_2(file_name: str, total_img: int, img_height: int, img_width: int
     tensor = transform(img_gray_mode)
 
     for idx in range(total_img - 1):
-        img_gray_mode2 = cv2.imread('Image_rgb' + file_name + ' {}'.format(idx + 2) + '.png', 0)
+        img_gray_mode2 = cv2.imread(dataset_path + '/Image_rgb' + file_name + ' {}'.format(idx + 2) + '.png', 0)
         img_gray_mode2[:line_width, :] = 0
         img_gray_mode2[:, :line_width] = 0
         img_gray_mode2[img_gray_mode.shape[0] - line_width:, :] = 0
@@ -275,25 +305,28 @@ def tensor_out_2(file_name: str, total_img: int, img_height: int, img_width: int
         tensor2 = transform(img_gray_mode2)
         tensor = torch.cat((tensor, tensor2))
     tensor = tensor.reshape(total_img, img_width, img_height)
-    return tensor
+    out_tensor = line_tensor * tensor
+    return out_tensor
 
 
-def vor_rgb_img_creator(total_img: int, rand_point_no: int, img_height: int, img_width: int, line_width: int,
-                        line_color: str) -> torch.Tensor:
+def vor_rgb_img_creator(dataset_folder: str, total_img: int, rand_point_no: int, img_height: int, img_width: int, line_width: int,
+                        line_color: str, color_list: list) -> torch.Tensor:
     '''
     Takes how many colored Voronoi images that will be constructed and creates the desired amount of random points for
     each Voronoi image. This function creates a second Voronoi image with the same parameters but with different colors.
-    Then, creates two two tensors for the first image and the second mock image. Returns the sum of these two tensors
+    Then, creates two tensors for the first image and the second mock image. Returns the sum of these two tensors
     which will give unique values for each cell. After creating the tensor, it deletes the mock images.This function is
     also calls the vor_reg_creator, vor_plotting, vor_plotting_rgb_mock, tensor_out_colored, and tensor_out_colored_mock
     functions.
 
+    :param dataset_folder: Path of the dataset folder
     :param total_img: Number of Voronoi images that will be constructed.
     :param rand_point_no: Number of random points.
     :param img_height: Height of the image.
     :param img_width: Width of the image.
     :param line_width: Width of the line, 0 means no line.
     :param line_color: Color of the line.
+    :param color_list: list of colors that the constructed image's Voronoi cells' will use
     :return tensor3: Tensor of the all images, return shape is (total_img, 3, img_width, img_height)
     '''
     for img_no in range(total_img):
@@ -303,69 +336,17 @@ def vor_rgb_img_creator(total_img: int, rand_point_no: int, img_height: int, img
             x_axis = random.uniform(0.0, img_width)
             coords.append([x_axis, y_axis])
         region_polys, boundary_shape = vor_reg_creator(img_height, img_width, coords)
-        vor_plotting_rgb("", rand_point_no, region_polys, img_width, img_height, line_width,
-                         line_color, img_no)
-        vor_plotting_rgb("_mock", rand_point_no, region_polys, img_width, img_height, line_width,
-                         line_color, img_no)
-    tensor2 = tensor_out_2("", total_img=total_img, img_height=img_height, img_width=img_width,
+        vor_plotting_rgb(dataset_folder, "", rand_point_no, region_polys, img_width, img_height, line_width,
+                         line_color, img_no, color_list)
+        vor_plotting_rgb(dataset_folder, "_mock", rand_point_no, region_polys, img_width, img_height, line_width,
+                         line_color, img_no, [])
+    tensor2 = tensor_out_2(dataset_folder, "", total_img=total_img, img_height=img_height, img_width=img_width,
                            line_width=line_width)
-    tensor2_mock = tensor_out_2("_mock", total_img=total_img, img_height=img_height, img_width=img_width,
+    tensor2_mock = tensor_out_2(dataset_folder, "_mock", total_img=total_img, img_height=img_height, img_width=img_width,
                                 line_width=line_width)
     tensor3 = (tensor2 + tensor2_mock) / 2
 
     for idx in range(total_img):
-        os.remove('Image_rgb_mock {}'.format(idx + 1) + '.png')
+        os.remove(dataset_folder + '/Image_rgb_mock {}'.format(idx + 1) + '.png')
+        os.remove(dataset_folder + '/line {}'.format(idx + 1) + '.png')
     return tensor2, tensor3
-
-
-def vor_plotting_rgb_mock(rand_point_no: int, region_polys: dict, img_width: int,
-                          img_height: int, line_width: int, line_color: str, img_no: int) -> None:
-    '''
-    This function saves a colored .png image with the desired line width, color, and name.
-
-    :param rand_point_no: Number of random points.
-    :param region_polys: Dictionary of polygon objects size of the number of random points. Each element of the
-      dictionary contains the coordinates of the corner points.
-    :param img_height: Height of the image.
-    :param img_width: Width of the image.
-    :param line_width: Width of the line, 0 means no line.
-    :param line_color: Color of the line.
-    :param img_no: The number that the current image will be saved with a name with that number.
-    '''
-
-    fig, ax = subplot_for_map(figsize=(img_width / 120, img_height / 120))
-    for point_no in range(rand_point_no):
-        points = []
-        for p_idx in range(len(region_polys[point_no].exterior.coords.xy[0])):
-            points.append([region_polys[point_no].exterior.coords.xy[0][p_idx],
-                           region_polys[point_no].exterior.coords.xy[1][p_idx]])
-        points = np.array(tuple(points))
-        plot_line(ax, points, linewidth=line_width, color=line_color)
-    rand_color = random.sample(list(matplotlib.colors.CSS4_COLORS), k=rand_point_no)
-    for point_no in range(rand_point_no):
-        plot_polygon_collection_with_color(ax, [region_polys[point_no]], color=rand_color[point_no])
-    plt.gca().set_axis_off()
-    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-    plt.margins(0, 0)
-    plt.gca().xaxis.set_major_locator(plt.NullLocator())
-    plt.gca().yaxis.set_major_locator(plt.NullLocator())
-    plt.savefig('Image_rgb_mock {}'.format(img_no + 1) + '.png', dpi=120, format='png', bbox_inches='tight',
-                pad_inches=0)
-    plt.close()
-
-
-def tensor_out_colored_mock(total_img: int, img_height: int, img_width: int) -> torch.Tensor:
-    '''
-    Takes the total number of colored mock images and returns the tensor of each image.
-
-    :param total_img: Filename of the image whose tensors will be returned.
-    :param img_height: Height of the image.
-    :param img_width: Width of the image.
-    :return tensor (): Tensor of the all images, return shape is (total_img, 3, img_width, img_height)
-    '''
-    output = img_to_tensor_colored('Image_rgb_mock {}'.format(1) + '.png', img_height, img_width)
-    for idx in range(total_img - 1):
-        output = torch.cat(
-            (output, img_to_tensor_colored('Image_rgb_mock {}'.format(idx + 2) + '.png', img_height, img_width)))
-    output = output.reshape(total_img, 3, img_width, img_height)
-    return output
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import matplotlib
+import torch
+import pandas as pd
 import Software_Practical
 
 # Press the green button in the gutter to run the script.
@@ -6,9 +9,13 @@ if __name__ == '__main__':
     img_height = 100
     img_width = 100
     total_img = 5
-    line_width = 1
-    line_color = 'blue'
+    line_width = 2
+    line_color = 'white'
     voronoi_cell_no = 8
+    base_colors = list(matplotlib.colors.BASE_COLORS)
+    css4_colors = list(matplotlib.colors.CSS4_COLORS)
+    color_list = [base_colors[0], base_colors[1], base_colors[2], base_colors[3], base_colors[4], base_colors[5],
+                  css4_colors[11], css4_colors[21]]
 
     '''
     tensor_1: Tensor of the all images, return shape is (total_img, 3, img_width, img_height)
@@ -20,12 +27,19 @@ if __name__ == '__main__':
     '''
 
     if rgb_img:
-        tensor_2, tensor_3 = Software_Practical.vor_rgb_img_creator(total_img=total_img, rand_point_no=voronoi_cell_no,
+        tensor_2, tensor_3 = Software_Practical.vor_rgb_img_creator("dataset", total_img=total_img, rand_point_no=voronoi_cell_no,
                                                                     img_height=img_height,
                                                                     img_width=img_width, line_width=line_width,
-                                                                    line_color=line_color)
-        tensor_1 = Software_Practical.tensor_out_colored(total_img=total_img, img_height=img_height,
+                                                                    line_color=line_color, color_list=color_list)
+        tensor_1 = Software_Practical.tensor_out_colored('dataset/Image_rgb', total_img=total_img, img_height=img_height,
                                                          img_width=img_width)
+        x_np = tensor_2[0].numpy()
+        x_df = pd.DataFrame(x_np)
+        x_df.to_csv('tensor2.csv')
+
+        x_np = tensor_3[0].numpy()
+        x_df = pd.DataFrame(x_np)
+        x_df.to_csv('tensor3.csv')
     else:
         Software_Practical.vor_bw_img_creator(total_img=total_img, rand_point_no=voronoi_cell_no, img_height=img_height,
                                               img_width=img_width, line_width=line_width, line_color=line_color)
