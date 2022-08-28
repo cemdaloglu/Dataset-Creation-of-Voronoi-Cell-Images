3f29314a21f74158b207be63f1b94c6be25a9ab8
Cem Daloglu <chemdaloglu@gmail.com>
2022-08-28 02:02:14 +0200

    update main

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import matplotlib
-import torch
 import pandas as pd
 import Software_Practical
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     rgb_img = True
-    img_height = 100
-    img_width = 100
+    img_height = 32
+    img_width = 32
     total_img = 5
-    line_width = 2
+    line_width = 1
     line_color = 'white'
-    voronoi_cell_no = 8
+    voronoi_cell_no = 6
     base_colors = list(matplotlib.colors.BASE_COLORS)
     css4_colors = list(matplotlib.colors.CSS4_COLORS)
     color_list = [base_colors[0], base_colors[1], base_colors[2], base_colors[3], base_colors[4], base_colors[5],
@@ -33,13 +32,6 @@ if __name__ == '__main__':
                                                                     line_color=line_color, color_list=color_list)
         tensor_1 = Software_Practical.tensor_out_colored('dataset/Image_rgb', total_img=total_img, img_height=img_height,
                                                          img_width=img_width)
-        x_np = tensor_2[0].numpy()
-        x_df = pd.DataFrame(x_np)
-        x_df.to_csv('tensor2.csv')
-
-        x_np = tensor_3[0].numpy()
-        x_df = pd.DataFrame(x_np)
-        x_df.to_csv('tensor3.csv')
     else:
         Software_Practical.vor_bw_img_creator(total_img=total_img, rand_point_no=voronoi_cell_no, img_height=img_height,
                                               img_width=img_width, line_width=line_width, line_color=line_color)
